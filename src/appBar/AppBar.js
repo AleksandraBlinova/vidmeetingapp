59bdadfc3adc208df29481b7401ed7feb5900002
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2023-04-06 14:11:51 +0300

    changed app bar

--- src/appBar/AppBar.js
+++ src/appBar/AppBar.js
@@ -1,31 +1,147 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import EventIcon from "@mui/icons-material/Event";
+import CallIcon from "@mui/icons-material/Call";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 
 const AppBarComponent = () => {
+  const [openSideBar, setOpenSideBar] = useState(false);
+  const handleOpenSideBar = () => {
+    setOpenSideBar(!openSideBar);
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
       {" "}
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{ backgroundColor: "#010002" }}>
           <Toolbar>
-            <IconButton sx={{ p: 2 }}>
-              <Avatar alt="Remy Sharp" />
-            </IconButton>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar alt="Remy Sharp" />
+              </IconButton>
+            </Tooltip>
             <Typography
               variant="h6"
               noWrap
               component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "block" },
+                paddingLeft: "10px",
+              }}
             >
               User Name
             </Typography>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem>
+                <Avatar /> Profile
+              </MenuItem>
+
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <EventIcon fontSize="small" />
+                </ListItemIcon>
+                Events
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContactsIcon fontSize="small" />
+                </ListItemIcon>
+                Contacts
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <NotificationsActiveIcon fontSize="small" />
+                </ListItemIcon>
+                Notifications
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <CallIcon fontSize="small" />
+                </ListItemIcon>
+                Calls
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Settings fontSize="small" />
+                </ListItemIcon>
+                Settings
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
             <Box>
-              <img src="./logo.png" style={{ width: "80px", height: "40px" }} />
+              <img
+                src="./logo.png"
+                style={{ width: "80px", height: "40px", float: "left" }}
+              />
             </Box>
           </Toolbar>
         </AppBar>
