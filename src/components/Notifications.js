bd9f9f492d22726f0780a5f5e499d89df4f9edbe
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2023-04-21 17:54:18 +0300

    change search in notif

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
-import { Form, Button, Radio, Space, Switch, Table } from "antd";
+import { Table, Input } from "antd";
 
 import AppBarComponent from "./AppBar";
 import "../styles/Notifications.css";
 
 import { dataSourceNotify } from "../data/notify-data";
-import { columns } from "../data/notify-data-table-columns";
+import { purple } from "@mui/material/colors";
+
+import Checkbox from "@mui/material/Checkbox";
 
 const Notifications = () => {
   const [bottom, setBottom] = useState("bottomRight");
+  const [searchedText, setSearchedText] = useState("");
 
   return (
     <div className="notify-container">
@@ -17,8 +20,74 @@ const Notifications = () => {
 
       <div className="notify-table-container">
         {" "}
+        <Input.Search
+          placeholder="Search here..."
+          style={{ width: "400px", marginBottom: "6px" }}
+          onSearch={(value) => setSearchedText(value)}
+          onChange={(e) => {
+            setSearchedText(e.target.value);
+          }}
+        />
         <Table
-          columns={columns}
+          columns={[
+            {
+              title: "RECEIVER",
+              dataIndex: "receiver",
+              filteredValue: [searchedText],
+              onFilter: (value, record) => {
+                return (
+                  String(record.receiver)
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) ||
+                  String(record.email)
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) ||
+                  String(record.title)
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) ||
+                  String(record.message)
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) ||
+                  String(record.date)
+                    .toLowerCase()
+                    .includes(value.toLowerCase())
+                );
+              },
+            },
+            {
+              title: "EMAIL",
+              dataIndex: "email",
+            },
+            {
+              title: "TITLE",
+              dataIndex: "title",
+            },
+            {
+              title: "MESSAGE",
+              dataIndex: "message",
+            },
+            {
+              title: "DATE",
+              dataIndex: "date",
+            },
+            {
+              title: "SENT",
+              dataIndex: "sent",
+              render: (_, { sent }) => (
+                <>
+                  <Checkbox
+                    checked={sent}
+                    sx={{
+                      color: purple[800],
+                      "&.Mui-checked": {
+                        color: purple[600],
+                      },
+                    }}
+                  />
+                </>
+              ),
+            },
+          ]}
           dataSource={dataSourceNotify}
           pagination={{
             position: [bottom],
